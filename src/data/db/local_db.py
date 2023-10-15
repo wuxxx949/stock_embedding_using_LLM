863296fb6bb0ee436c37b932ecd294cb416ff949
wuxxx949 <timnaka123@gmail.com>
2023-10-15 16:19:12 -0400

    prob db

--- src/data/db/local_db.py
+++ src/data/db/local_db.py
@@ -1,17 +1,21 @@
 import os
 import re
 import sqlite3
+from importlib.resources import files
 from sqlite3 import Connection
-
+from typing import Dict
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
-from src.data.db.queries import CREATE_BUSINESS_DESC_QUERY
+from src.data.db.queries import (CREATE_BUSINESS_DESC_DEF,
+                                 CREATE_GICS_MAPPING_DEF, CREATE_GICS_PROB_DEF)
 from src.meta_data import get_meta_data
 from src.model.embedding import bert_embedding, sbert_embedding
 from src.model.model_data import filter_files, make_input_data
+from src.model.mutliple_gics import top_classes
 
 
-def create_embedding_table_query(tbl_name: str, length: int) -> str:
+def create_embedding_table_def(tbl_name: str, length: int) -> str:
     """create local embedding table
 
     Args:
@@ -29,6 +33,56 @@ def create_embedding_table_query(tbl_name: str, length: int) -> str:
     last_line = f'ticker TEXT NOT NULL);'
 
     return first_line + col_def + last_line
+
+
+def create_prob_table_def(tbl_name: str) -> str:
+    """query to create prob table
+
+    Args:
+        tbl_name (str): talbe name
+
+    Returns:
+        str: table def
+    """
+    return CREATE_GICS_PROB_DEF.format(tbl_name)
+
+def create_gics_mapping(
+    key_series: pd.Series,
+    value_series:pd.Series
+) -> Dict[str, str]:
+    return {k: v for k, v in zip(key_series, value_series)}
+
+def upload_ticker_gics_mapping() -> str:
+    fpath = os.path.join(files('src'), 'data/raw_data/ticker_to_gics.csv')
+    gics_df = pd.read_csv(fpath, names=['ticker', 'gics'], dtype=str)
+
+    fpath = os.path.join(files('src'), 'data/raw_data/gics_mapping.csv')
+    mapping_df = pd.read_csv(fpath, dtype=str)
+
+    subindustry_mapping = create_gics_mapping(
+        mapping_df['subindustry_code'], mapping_df['subindustry']
+    )
+
+    industry_mapping = create_gics_mapping(
+        mapping_df['subindustry_code'], mapping_df['industry']
+    )
+
+    industry_group_mapping = create_gics_mapping(
+        mapping_df['subindustry_code'], mapping_df['industry_group']
+    )
+
+    sector_mapping = create_gics_mapping(
+        mapping_df['subindustry_code'], mapping_df['sector']
+    )
+    gics_df['sector'] = gics_df['gics'].map(sector_mapping)
+    gics_df['industry_group'] = gics_df['gics'].map(industry_group_mapping)
+    gics_df['industry'] = gics_df['gics'].map(industry_mapping)
+    gics_df['sub_industry'] = gics_df['gics'].map(subindustry_mapping)
+    gics_df['ticker'] = gics_df['ticker'].str.lower()
+    del gics_df['gics']
+
+    conn.execute(CREATE_GICS_MAPPING_DEF)
+    gics_df.to_sql('gics_mapping', conn, index=False, if_exists='replace')
 
 
 def upload_bert_embedding(conn: Connection, num_seg: int) -> None:
@@ -66,15 +120,66 @@ def upload_business_desc(conn: Connection) -> None:
     desc_df.to_sql('business_desc', conn, index=False, if_exists='replace')
 
 
+def upload_prob(
+    conn: Connection,
+    model_type: str,
+    gics_type: str,
+    chunk_length: int,
+    num_seg: int,
+    C: float,
+    seed: int = 42
+    ) -> None:
+
+    tbl_name = f'{model_type}_{chunk_length * num_seg}_prob'
+    conn.execute(create_prob_table_def(tbl_name))
+
+    model = LogisticRegression(
+        multi_class='multinomial',
+        solver='lbfgs',
+        C=C,
+        penalty='l2',
+        random_state=seed
+    )
+
+    texts, labels, tickers = make_input_data(gics_type)
+    if model_type == 'bert':
+        embeddings = bert_embedding(
+            model_name=get_meta_data()['BERT_MODEL_DIR'],
+            texts=texts,
+            chunk_length=chunk_length,
+            num_seg=3
+        )
+
+    model.fit(embeddings, labels)
+    prob_array = model.predict_proba(embeddings)
+
+    gics_pred = top_classes(
+        prob_array=prob_array,
+        tickers=tickers,
+        gics_type=gics_type,
+        top_n=5
+    )
+
+    gics_pred.rename({gics_type: 'classification'}, axis=1)
+    gics_pred.to_sql(tbl_name, conn, index=False, if_exists='replace')
+
 
 if __name__ == '__main__':
-    db_dir = os.path.dirname(os.path.realpath(__file__))
+    db_dir = os.path.join(files('src'), 'data/db')
     conn = sqlite3.connect(os.path.join(db_dir, 'dash.db'))
 
-    tbl_def = create_embedding_table_query('bert_embedding_1536', length=768)
+    upload_prob(
+        conn,
+        model_type='bert',
+        gics_type='industry',
+        chunk_length=512,
+        num_seg=3,
+        C=0.05
+    )
+
+    tbl_def = create_embedding_table_def('bert_embedding_1536', length=768)
     conn.execute(tbl_def)
-    conn.execute(CREATE_BUSINESS_DESC_QUERY)
+    conn.execute(CREATE_BUSINESS_DESC_DEF)
 
     upload_business_desc(conn)
     upload_bert_embedding(conn, num_seg=3)
-
--- src/data/db/queries.py
+++ src/data/db/queries.py
@@ -1,6 +1,24 @@
-CREATE_BUSINESS_DESC_QUERY = """
-CREATE TABLE IF NOT EXISTS business_desc (
-    ticker TEXT PRIMARY KEY,
-    business TEXT NOT NULL
-);
+CREATE_BUSINESS_DESC_DEF = """
+    CREATE TABLE IF NOT EXISTS business_desc (
+        ticker TEXT PRIMARY KEY,
+        business TEXT NOT NULL
+    );
+"""
+
+CREATE_GICS_PROB_DEF = """
+    CREATE TABLE IF NOT EXISTS {} (
+        ticker TEXT,
+        classification TEXT,
+        prob FLOAT
+    );
+"""
+
+CREATE_GICS_MAPPING_DEF = """
+    CREATE TABLE IF NOT EXISTS gics_mapping (
+        ticker TEXT,
+        sector TEXT,
+        industry_group TEXT,
+        industry TEXT,
+        subindustry TEXT
+    );
 """
